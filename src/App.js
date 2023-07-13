6e559ea48875181ca2c2a5ee526a254c5d1f8fbf
Tushar Talesara <talesara17413824@gmail.com>
2023-07-13 13:04:28 +0530

    Added Validation

--- src/App.js
+++ src/App.js
@@ -5,31 +5,48 @@ import axios from 'axios'
 function App() {
 
   /* React hooks for each attribute */
-  const [name,setName]=useState('')
-  const [branch,setBranch]=useState('')
-  const [email,setEmail]=useState('')
-  const [number,setNumber]=useState('')
+  const [name, setName] = useState('')
+  const [branch, setBranch] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+
+  /* Function to check if email is valid */
+  function isValidEmail(email) {
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  }
 
   /* Function to handle submit */
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault()
 
+    /* Validation */
+    //Email
+    if (!isValidEmail(email)) {
+      alert('Email is invalid')
+      return
+    }
+    //Number
+    if (!(number.match('[0-9]{10}'))) {
+      alert('Please provide valid phone number');
+      return
+    }
+
     //Data
-    const data={
-      name:name,
-      branch:branch,
-      email:email,
-      number:number
+    const data = {
+      name: name,
+      branch: branch,
+      email: email,
+      number: number
     }
 
     /* Sending the data to backend (FireBase) */
-    axios.post('http://localhost:5000/submit',data)
-          .then((res)=>{
-            console.log(res)
-          })
-          .catch((err)=>{
-            console.log(err)
-          })
+    axios.post('http://localhost:5000/submit', data)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
 
     /* Creating a new object to store the data */
     setName('')
@@ -45,11 +62,11 @@ function App() {
         <ul className="form-list">
           <li className="form-item">
             <label htmlFor="name">Name *</label>
-            <input type="text" id="name" onChange={(e)=>{setName(e.target.value)}} value={name} required />
+            <input type="text" id="name" onChange={(e) => { setName(e.target.value) }} value={name} required />
           </li>
           <li className="form-item">
             <label htmlFor="branch">Branch *</label>
-            <select id="branch" value={branch} onChange={(e)=>{setBranch(e.target.value)}} required>
+            <select id="branch" value={branch} onChange={(e) => { setBranch(e.target.value) }} required>
               <option hidden />
               <option>	Architecture , Planning and Design</option>
               <option>Bio-Chemical Engineering</option>
@@ -72,11 +89,11 @@ function App() {
           </li>
           <li className="form-item">
             <label htmlFor="email">Email *</label>
-            <input type="text" id="email" onChange={(e)=>{setEmail(e.target.value)}} value={email} required />
+            <input type="text" id="email" onChange={(e) => { setEmail(e.target.value) }} value={email} required />
           </li>
           <li className="form-item">
-            <label htmlFor="number">Number *</label>
-            <input type='text' id="number" onChange={(e)=>{setNumber(e.target.value)}} value={number} required />
+            <label htmlFor="number">Phone Number *</label>
+            <input type='text' id="number" onChange={(e) => { setNumber(e.target.value) }} value={number} required />
           </li>
           <button className="btn" type='submit'>Submit</button>
         </ul>
